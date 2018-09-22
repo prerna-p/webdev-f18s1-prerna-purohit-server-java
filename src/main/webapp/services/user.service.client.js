a8b050b551509dd270c2f08294da9f286743ff30
prerna-p <prernasocialid@gmail.com>
2018-09-22 13:07:38 -0400

    created user.service.client.js

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -1,36 +1,35 @@
 (function () {
 
-}
 function AdminUserServiceClient() {
     this.createUser = createUser;
     this.findAllUsers = findAllUsers;
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'http://localhost:8080/api/user';
     var self = this;
-    function createUser(user, callback) { … }
-    function findAllUsers(callback) { … }
-    function findUserById(userId, callback) { … }
-    function updateUser(userId, user, callback) { … }
-    function deleteUser(userId, callback) { … }
 
-    function createUser(user){
-        
-    }
-    function findAllUsers(){
-
-    }
-    function findUserById(id){
+    function createUser(user, callback) {
 
     }
 
-    function updateUser(id, user){
+    function findAllUsers() {
+        return [
+            {username: 'alice'},
+            {username: 'bob '},
+            {username: 'charly'}
+        ]
+    }
+
+    function findUserById(userId, callback) {
 
     }
 
-    function deleteUser(id){
+    function updateUser(userId, user, callback) {
 
     }
 
-})
+    function deleteUser(userId, callback) {
+
+    }
+    }
+})()
